fcd222ca205d50fe99d02e1f649c608cc4956a03
hilschernetpi <netpi@hilscher.com>
2021-04-07 10:25:17 +0000

    starting dbus correctly

--- init.d/entrypoint.sh
+++ init.d/entrypoint.sh
@@ -6,11 +6,26 @@ if [[ -z `grep "docker0" /proc/net/dev` ]]; then
   exit 143
 fi
 
+# check if dbus address is already defined -> then dbus-deamon of host shall be used
+if [[ ! -S "/var/run/dbus/system_bus_socket" ]]; then
+   # else start a dbus daemon instance in the container
+   /etc/init.d/dbus start
+   touch dbus_in_container_started
+elif  [[ -e dbus_in_container_started ]]; then
+   # start a dbus daemon instance in the container
+   /etc/init.d/dbus start
+fi
+
 pidbt=0
 
 # catch signals as PID 1 in a container
 # SIGNAL-handler
 term_handler() {
+
+  echo "terminating dbus ..."
+  if [[ ! -S "/var/run/dbus/system_bus_socket" ]]; then
+    /etc/init.d/dbus stop
+  fi
 
   exit 143; # 128 + 15 -- SIGTERM
 }
@@ -30,7 +45,7 @@ fi
 
 #check if we have a user V2 management running
 httpUrl='https://127.0.0.1/cockpit/login'
-rep=$(curl -k -s $httpUrl)
+rep=$(curl -k -s $httpUrl -H 'cookie: cockpit=deleted')
 if [[ $rep == *'Authentication failed'* ]]; then
   sed -i -e 's+//adminAuth: {+adminAuth: require("./user-authentication_v2.js"),\n    //adminAuth: {+' /usr/lib/node_modules/node-red/settings.js
 else
@@ -39,7 +54,7 @@ fi
 
 
 # start Node-RED as background task
-/usr/bin/node-red &
+/usr/bin/node-red flows.json &
 
 # wait forever not to exit the container
 while true
